e30611abb7c11328658211e94c76712baa85dffb
sality <31968622+xameleon4yk@users.noreply.github.com>
2026-04-01 16:55:17 +0300

    Update update_teamviewer.bat

--- update_teamviewer.bat
+++ update_teamviewer.bat
@@ -1,7 +1,7 @@
 c:
 cd c:\program files
 md rdpwrap
-powershell.exe -command wget "https://download.teamviewer.com/download/TeamViewer_Setup.exe" -OutFile 'C:\program files\rdpwrap\TeamViewer_Setup.exe'
+powershell.exe -command wget "https://download.teamviewer.com/download/version_15x/15.64.5/TeamViewer_Setup_x64.exe" -OutFile 'C:\program files\rdpwrap\TeamViewer_Setup.exe'
 cd rdpwrap
 tskill teamviewer /a
 powershell.exe Start-Process -Wait -FilePath "TeamViewer_Setup.exe"  -ArgumentList "/S", "--grant-easy-access"
