c9e08702bae243492c46c5e5c703db39f1897077
sality <31968622+xameleon4yk@users.noreply.github.com>
2026-04-02 12:43:12 +0300

    Update uninstall_teamviewer.bat

--- uninstall_teamviewer.bat
+++ uninstall_teamviewer.bat
@@ -4,4 +4,5 @@ cd rdpwrap
 tskill teamviewer /a
 reg export "HKEY_LOCAL_MACHINE\SOFTWARE\TeamViewer" "C:\program files\rdpwrap\backup_tv.reg" /y
 reg export "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TeamViewer" "C:\program files\rdpwrap\backup_tv_w.reg" /y
-powershell.exe Start-Process -Wait -FilePath "TeamViewer_Setup.exe"  -ArgumentList "/S"
+Start-Process -FilePath "C:\Program Files (x86)\TeamViewer\uninstall.exe" -ArgumentList "/S" -Wait
+Start-Process -FilePath "C:\Program Files\TeamViewer\uninstall.exe" -ArgumentList "/S" -Wait
