219bc6e8181af57203caad09b58c339791449fd2
sality <31968622+xameleon4yk@users.noreply.github.com>
2026-04-14 10:48:38 +0300

    Update powerscheme.bat

--- powerscheme.bat
+++ powerscheme.bat
@@ -1,5 +1,3 @@
-for /f "tokens=3" %a in ('powercfg /list ^| findstr "GUID"') do (
-  powercfg /setacvalueindex %a 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 5
-  powercfg /setdcvalueindex %a 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 5
-)
+powercfg /setacvalueindex 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 5
+powercfg /setdcvalueindex 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 5
 powercfg /s SCHEME_CURRENT
