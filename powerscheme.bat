27a16da52afb71b3e0115083f830eb910dcd67e1
sality <31968622+xameleon4yk@users.noreply.github.com>
2026-04-14 10:06:33 +0300

    Create powerscheme.bat

---
+++ powerscheme.bat
@@ -0,0 +1,2 @@
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR 893dee8e-2bef-41e0-89c6-b55d0929964c 5
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR 893dee8e-2bef-41e0-89c6-b55d0929964c 5
